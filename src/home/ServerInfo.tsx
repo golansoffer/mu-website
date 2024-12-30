b3a913f0dea44418549eeaf31daab45238dd0549
Golan Soffer <gsoffer550@gmail.com>
2024-12-30 11:37:03 +0200

    //

--- src/home/ServerInfo.tsx
+++ src/home/ServerInfo.tsx
@@ -3,23 +3,16 @@ import styles from './ServerInfo.module.css';
 
 export function ServerInfo() {
     return <section className={styles.root}>
-        <article>
+        <article className={styles.container}>
             <div>
                 <h1>About.</h1>
                 <p>
-                    We are a team of top-tier developers with over ten years of experience, dedicated to merging modern
-                    technology with the classic, <strong>nostalgic MU Online experience.</strong> While we’re constantly
-                    refining
-                    features to keep the game robust and up-to-date, we remain committed to preserving the old-school
-                    feel that fans love.
-                </p>
-                <p>
-                    <strong>This server is—and always will be—strictly non-pay-to-win</strong>. We don’t believe in
-                    giving players an
-                    unfair advantage through money. Donations are completely optional and only help with server upkeep.
-                    The few purchasable items, like extra inventory space or low-tier boxes (Heart of Love, Box of
-                    Luck), are purely for convenience. MU remains free for all—no pressure, no paywalls, just pure
-                    gameplay.
+                    We’re a team of real-life software engineers and devoted MU fans, merging <strong>classic MU
+                    nostalgia</strong> with
+                    modern polish. Our <strong>strictly non-P2W server</strong> never sells exp or drop boosts, ensuring
+                    fair, old-school
+                    gameplay. While optional, non-competitive purchases help sustain our cloud services, preserving the
+                    authentic MU experience remains our top priority.
                 </p>
             </div>
             <div className={styles.config_container}>
