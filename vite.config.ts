8499472a3bba3a54575a87962f0649b0aebea813
Golan Soffer <gsoffer550@gmail.com>
2024-12-31 15:06:44 +0200

    //

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import {TanStackRouterVite} from '@tanstack/router-plugin/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/mu-website/',
     plugins: [TanStackRouterVite({}), react()],
 })
