d2a771a53c601201c379edc109a2163b70b94b94
golansoffer <Gsoffer550@gmail.com>
2025-01-11 01:48:09 +0200

    Refactor Vite configuration to ensure API URL is correctly formatted as a template string

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import {TanStackRouterVite} from '@tanstack/router-plugin/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [TanStackRouterVite({}), react()],
-    define: { VITE_API_URL: process.env.VITE_API_URL }
+    define: { VITE_API_URL: `${process.env.VITE_API_URL}` }
 })
