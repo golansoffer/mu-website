8a0f92231d2a8cd6cd127bdc985ef24fa1284f23
golansoffer <Gsoffer550@gmail.com>
2025-01-11 01:36:55 +0200

    Update Vite configuration to include API key and refactor API URL handling

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,5 @@ import {TanStackRouterVite} from '@tanstack/router-plugin/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [TanStackRouterVite({}), react()],
+    define: { VITE_API_URL: process.env.VITE_API_URL, }
 })
