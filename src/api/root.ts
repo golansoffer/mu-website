5d6ff24459b2c0c5e524bb2dea8ed41f414c7193
golansoffer <Gsoffer550@gmail.com>
2025-01-11 03:02:44 +0200

    Refactor fetcher function in API to use updated HTTPS URL for API requests

--- src/api/root.ts
+++ src/api/root.ts
@@ -1,8 +1,9 @@
 //const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5565/api';
 
 export function fetcher<T>(url: string, options?: RequestInit) {
+    console.log('@@',import.meta.env.VITE_API_URL);
     const token = localStorage.getItem('token');
-    return fetch(`http://34.159.179.66/api/${url}`, {
+    return fetch(`https://mu-bff-10300237159.europe-west9.run.app/api/${url}`, {
         headers: {
             'Content-Type': 'application/json',
             'Authorization': token ? `Bearer ${token}` : '',
