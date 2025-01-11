8772fdd9f68297100b9216d1a2c3f088c56828b9
golansoffer <Gsoffer550@gmail.com>
2025-01-11 02:12:44 +0200

    Refactor API URL in fetcher function to use hardcoded IP address, removing reliance on environment variable

--- src/api/root.ts
+++ src/api/root.ts
@@ -1,8 +1,8 @@
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5565/api';
+//const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5565/api';
 
 export function fetcher<T>(url: string, options?: RequestInit) {
     const token = localStorage.getItem('token');
-    return fetch(`${API_URL}/api/${url}`, {
+    return fetch(`http://34.159.179.66/api/${url}`, {
         headers: {
             'Content-Type': 'application/json',
             'Authorization': token ? `Bearer ${token}` : '',
