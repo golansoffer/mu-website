d36fae85792127f417accca5dc71ae90519c9ff2
golansoffer <Gsoffer550@gmail.com>
2025-01-11 02:11:49 +0200

    Refactor fetcher function in API to include 'api' prefix in URL path

--- src/api/root.ts
+++ src/api/root.ts
@@ -2,7 +2,7 @@ const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5565/api';
 
 export function fetcher<T>(url: string, options?: RequestInit) {
     const token = localStorage.getItem('token');
-    return fetch(`${API_URL}/${url}`, {
+    return fetch(`${API_URL}/api/${url}`, {
         headers: {
             'Content-Type': 'application/json',
             'Authorization': token ? `Bearer ${token}` : '',
