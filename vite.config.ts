0560fa9cb68d9cff96ac8d6a3fe6b6ef3a9e29d5
golansoffer <Gsoffer550@gmail.com>
2025-01-11 12:30:57 +0200

    Enhance Vite configuration by adding build options for manual chunking and server settings for proxy and auto-opening. This improves the development experience and optimizes the build process.

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,16 @@ import {TanStackRouterVite} from '@tanstack/router-plugin/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [TanStackRouterVite({}), react()],
-    define: { VITE_API_URL: process.env.VITE_API_URL }
+    define: { VITE_API_URL: process.env.VITE_API_URL },
+    build: {
+        rollupOptions: {
+          output: {
+            manualChunks: undefined
+          }
+        }
+      },
+      server: {
+        proxy: {},
+        open: true
+      }
 })
